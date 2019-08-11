6f39468bcd8c48e2617450d8ee7cde75190527f4
adel ak <adoolak@gmail.com>
2019-08-11 21:34:51 +0300

    ignore config

--- src/components/config.js
+++
@@ -1,9 +0,0 @@
-const apiKey = '?api_key=485d4dc0b0bda15539f537770b92b13a';
-const secretKey = '59a6ead81d85665a';
-const endPoint = `https://www.flickr.com/services/rest/${apiKey}&format=json&nojsoncallback=1&per_page=24`;
-
-export {
-    apiKey,
-    secretKey,
-    endPoint
-};
